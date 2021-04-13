be135a4d1fba271306a81364680abd6048205ef4
Aleck Zander <aleckzts@gmail.com>
2021-04-13 17:46:52 -0300

    all tests ok

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const user_id_string = user_id as string;
+      const all = this.listAllUsersUseCase.execute({ user_id: user_id_string });
+
+      return response.json(all);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
